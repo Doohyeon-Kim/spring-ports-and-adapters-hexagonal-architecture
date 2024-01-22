8437a84d296f53c899dd1b4e34872549751f1c48
Doohyeon Kim <me@doohyeon.kim>
2024-01-22 11:56:29 +0900

    Addapt Security Config

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO ACCOUNT(account_number, balance, name)
-VALUES ("01-2345-6789", "1000", "Doohyeon Kim");
+VALUES ('01-2345-6789', '1000', 'Doohyeon Kim');
 
 INSERT INTO ACCOUNT(account_number, balance, name)
-VALUES ("02-2345-6789", "2000", "Mr. Target");
+VALUES ('02-2345-6789', '2000', 'Mr. Target');
